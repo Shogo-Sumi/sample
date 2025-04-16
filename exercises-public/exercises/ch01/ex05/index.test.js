083c7461d2d2dc3bf8e71c9c5b9b0b1ec91ab4f6
Shogo <shogo.sumi@jp.ricoh.com>
2025-04-16 15:58:15 +0900

    [Add] test.txt

--- exercises-public/exercises/ch01/ex05/index.test.js
+++ exercises-public/exercises/ch01/ex05/index.test.js
@@ -1,20 +1,24 @@
-import { abs, sum, factorial } from "./index.js";
+//import { abs, sum, factorial } from "./index.js";
+import { abs } from "./index.js";
+
+//./index.jsからabs,sum,factorialモジュールをインポート
 
 // TypeScript の場合は以下:
 // import { abs, sum, factorial } from "./index.ts";
 
 describe("math", () => {
-  describe("abs", () => {
-    it("returns same value when positive value given", () => {
-      expect(abs(42)).toBe(42);
+	//関連するテストをmathとしてまとめたメソッド
+  describe("abs", () => {//abs関数のテストを実行するメソッド
+    it("returns same value when positive value given", () => {//testコードの説明。正の値が入力された場合、同じ値を返す
+      expect(abs(42)).toBe(42);//テスト結果の確認。abs関数へ42が入力された場合、42が返るか
     });
 
-    it("returns negated value when negative value given", () => {
-      expect(abs(-42)).toBe(42);
+    it("returns negated value when negative value given", () => {//testコードの説明。負の値が入力された場合、反転した値を返す
+      expect(abs(-42)).toBe(42);//テスト結果の確認。abs関数へ-42が入力された場合、42が返るか
     });
 
-    it("returns zero value when zero given", () => {
-      expect(abs(0)).toBe(0);
+    it("returns zero value when zero given", () => {//testコードの説明。０が入力された場合、0が返るか。
+      expect(abs(0)).toBe(0);//テスト結果の確認。abs関数へ０が入力された場合、０が返るか。
     });
   });
 
