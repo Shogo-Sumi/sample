e8cd10a838bfba30b6401a358dd5c05357bbd547
shogo <sgsm95@gmail.com>
2025-04-21 14:01:03 +0900

    [Add]index.js index.test.js

--- exercises/ch01/ex05/index.js
+++ exercises/ch01/ex05/index.js
@@ -16,3 +16,19 @@ export function abs(x) {
 }
 
 
+export function sum(array) {
+	let sum = 0;
+	for(let x of array) {
+		sum += x;
+	}
+	return sum;
+}
+
+export function factorial(n) { // 階乗を計算する関数。
+	let product = 1; // 1からスタート。
+	while(n > 1) { // ()中の式がtrueの間は{}中の文を繰り返す。
+		product *= n; // product = product * n; の短縮表記。
+		n--; // n = n - 1 の短縮表記。
+	} // ループの最後。
+return product; // 計算結果を返す。
+}
--- exercises/ch01/ex05/index.test.js
+++ exercises/ch01/ex05/index.test.js
@@ -1,11 +1,12 @@
 //import { abs, sum, factorial } from "./index.js";
-import { abs } from "./index.js";
+import { abs,sum,factorial } from "./index.js";
 
 //./index.jsからabs,sum,factorialモジュールをインポート
 
 // TypeScript の場合は以下:
 // import { abs, sum, factorial } from "./index.ts";
 
+//test abs
 describe("math", () => {
 	//関連するテストをmathとしてまとめたメソッド
   describe("abs", () => {//abs関数のテストを実行するメソッド
@@ -21,6 +22,49 @@ describe("math", () => {
       expect(abs(0)).toBe(0);//テスト結果の確認。abs関数へ０が入力された場合、０が返るか。
     });
   });
-
-  // 以下に sum, factorial のテストを記載せよ
 });
+
+
+// 以下に sum, factorial のテストを記載せよ
+
+//test sum
+describe("math", () => {
+        //関連するテストをmathとしてまとめたメソッド
+  describe("sum", () => {//sum関数のテストを実行するメソッド
+    it("send positive value", () => {//正の値が入力された場合、正の合計値を返す
+      let x = [ 1,2,3,4,5]
+      expect(sum(x)).toBe(15);
+      console.log(sum(x));
+    });
+    it("send negated value", () => {//負の値が入力された場合、負の合計値を返す
+      let y = [ -1,-2,-3,-4,-5]
+      expect(sum(y)).toBe(-15);
+      console.log(sum(y));
+    });
+    it("send positive and negative values", () => {//正の値と、負の値の合計値を入力。
+      let z = [ -1,2,-3,4,-5]
+      expect(sum(z)).toBe(-3);//テスト結果の確認。入力された正の値と負の値の合計値を返すか。
+　　　console.log(sum(z));
+    });
+  });
+});
+
+//test factorial
+describe("math", () => {
+        //関連するテストをmathとしてまとめたメソッド
+  describe("factorial", () => {//factorial関数のテストを実行するメソッド
+    it("send positive value", () => {//正の値が入力された場合、正の階乗を返す
+      expect(factorial(5)).toBe(120);//負の値が入力された場合、負の合計値を返す
+      console.log(factorial(5));
+    });
+    it("send negated value", () => {
+      console.log(factorial(-5));
+      console.log("Please input positive value");
+    });
+    it("send 0 value", () => {//正の値と、負の値の合計値を入力。
+      expect(factorial(0)).toBe(1);//テスト結果の確認。入力された正の値と負の値の合計値を返すか。
+　　　console.log(factorial(0));
+    });
+  });
+});
+
