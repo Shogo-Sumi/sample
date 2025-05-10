27fb58404ded2f1551aa8b0fe9948cc0597265f1
shogo <sgsm95@gmail.com>
2025-05-10 10:00:07 +0900

    [Add]ex04

---
+++ exercises/ch02/ex04/hello.js
@@ -0,0 +1,1 @@
+console.log("\0\b\t\nHello,\v\f\r World!");
--- exercises/ch02/ex05/index.test.js
+++ exercises/ch02/ex05/index.test.js
@@ -1,5 +1,5 @@
-import { promisify } from "util";
-import { exec } from "child_process";
+import { promisify } from "util";//非同期処理の完了、失敗の結果を返す
+import { exec } from "child_process";//文字列を引数にとり、指定された文字列に対して正規表現による検索を実行
 
 describe("charfreq", () => {
   it("変更前と結果が同じであること", async () => {
