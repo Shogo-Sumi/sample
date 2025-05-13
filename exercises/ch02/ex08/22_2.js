66048682769df323394ac98144a628e16f3e2037
shogo <sgsm95@gmail.com>
2025-05-13 14:24:39 +0900

    [Add]ex08

---
+++ exercises/ch02/ex08/22_2.js
@@ -0,0 +1,16 @@
+// Acornをインポート
+const acorn = require('acorn');
+
+// 解析対象のJavaScriptコード
+const code = `
+let a; a = 3; console.log(a);
+ `;
+
+// Acornでコードを解析し、ASTを生成
+const ast = acorn.parse(code, {
+  ecmaVersion: 'latest', // 使用するECMAScriptのバージョン（例: 'latest'で最新）
+  sourceType: 'script'   // 'script'（通常のスクリプト）または'module'（ESモジュール）
+});
+
+// 生成されたASTをコンソールに出力
+console.log(JSON.stringify(ast, null, 2));
