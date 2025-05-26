d2548a5079a78cdf2368464d09d3f462fce37631
shogo <sgsm95@gmail.com>
2025-05-26 17:40:30 +0900

    [Add]ex09

---
+++ exercises/ch03/ex09/index.test.js
@@ -0,0 +1,8 @@
+test("テスト名を適切につけてなさい", () => {
+    let points = [{x:1, y:2}, {x:3, y:4}];
+    /* 左辺を埋める */ = points;
+    expect(x1).toBe(1);
+    expect(y1).toBe(2);
+    expect(x2).toBe(3);
+    expect(y2).toBe(4);
+});
