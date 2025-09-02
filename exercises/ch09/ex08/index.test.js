37d1b58d30a9417ff811a2cf9f1764ab3a746163
Shogo <shogo.sumi@jp.ricoh.com>
2025-09-02 13:30:21 +0900

    [Add]ex08

--- exercises/ch09/ex08/index.test.js
+++ exercises/ch09/ex08/index.test.js
@@ -25,4 +25,60 @@ describe("AlarmClock Test", () => {
     expect(clock.setAlarm()).toStrictEqual("none");
     expect(clock.getState()).toStrictEqual("snoozing");
   });
+  it("Event 2.canelAlarm", () => {
+    clock.setState("normal")
+    expect(clock.cancelAlarm()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("alarmSet")
+    expect(clock.cancelAlarm()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("alarmSounding")
+    expect(clock.cancelAlarm()).toStrictEqual("stopAlarm");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("snoozing")
+    expect(clock.cancelAlarm()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+  });
+  it("Event 3.reachedToAlarmTime", () => {
+    clock.setState("normal")
+    expect(clock.reachedToAlarmTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("alarmSet")
+    expect(clock.reachedToAlarmTime()).toStrictEqual("soundAlarm");
+    expect(clock.getState()).toStrictEqual("alarmSounding");
+    clock.setState("alarmSounding")
+    expect(clock.reachedToAlarmTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("alarmSounding");
+    clock.setState("snoozing")
+    expect(clock.reachedToAlarmTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("snoozing");
+  });
+  it("Event 4.snooze", () => {
+    clock.setState("normal")
+    expect(clock.snooze()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("alarmSet")
+    expect(clock.snooze()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("alarmSet");
+    clock.setState("alarmSounding")
+    expect(clock.snooze()).toStrictEqual("stopAlarm");
+    expect(clock.getState()).toStrictEqual("snoozing");
+    clock.setState("snoozing")
+    expect(clock.snooze()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("snoozing");
+  });
+  it("Event 5.elapseSnoozeTime", () => {
+    clock.setState("normal")
+    expect(clock.elapseSnoozeTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("normal");
+    clock.setState("alarmSet")
+    expect(clock.elapseSnoozeTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("alarmSet");
+    clock.setState("alarmSounding")
+    expect(clock.elapseSnoozeTime()).toStrictEqual("none");
+    expect(clock.getState()).toStrictEqual("alarmSounding");
+    clock.setState("snoozing")
+    expect(clock.elapseSnoozeTime()).toStrictEqual("soundAlarm");
+    expect(clock.getState()).toStrictEqual("alarmSounding");
+  });
 });
